ec9f04323a2012b430642cb4eab66025f1a53ea5
dalilamoruso <162448426+dalilamoruso@users.noreply.github.com>
2024-04-29 15:27:21 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 function doLogin()  {
   var username = document.getElementById("username").value;
-  var psw = document.getElementById("password").value;
-  if(username == "" || psw == "") {
+  var password = document.getElementById("password").value;
+  if(username == "" || password == "") {
     alert("Username e password sono obbligatori")
   }
 }
